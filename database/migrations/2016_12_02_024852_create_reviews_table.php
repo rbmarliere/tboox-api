3c3c953cf07a53fc3ac8fb2e501c4c002bf13a64
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-02 01:11:12 -0200

    books, collections & reviews tables migrations

---
+++ database/migrations/2016_12_02_024852_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('collection_id')->unsigned();
+            $table->foreign('collection_id')->references('id')->on('collections');
+            $table->integer('rating');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
