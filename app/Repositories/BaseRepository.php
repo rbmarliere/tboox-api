8bf509d86569716c8fc7f9beea517612652c407f
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-04 16:26:35 -0200

    adding the base repository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements Repository
+{
+    protected $model;
+
+    public function createOrFail(array $fields) : integer
+    {
+        return $this->model
+            ->create($fields);
+    }
+
+    public function destroyOrFail(integer $uuid) : integer
+    {
+        $deleted = $this->show($uuid);
+
+        return $deleted
+            ->destroy();
+    }
+
+    public function index() : Collection
+    {
+        return $this->model
+            ->get();
+    }
+
+    public function show(integer $uuid) : Model
+    {
+        return $this->model
+            ->where('uuid', $uuid)
+            ->firstOrFail();
+    }
+}
+
