b8015e0b087edc2c0e1bd944be3c0e1e7444206f
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-04 16:13:06 -0200

    adding the api serializer

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -2,11 +2,14 @@
 
 namespace App\Repositories;
 
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
 interface Repository
 {
-    public function createOrFail(array $fields);
-    public function destroyOrFail(integer $uuid);
-    public function index();
-    public function show(integer $uuid);
+    public function createOrFail(array $fields) : integer;
+    public function destroyOrFail(integer $uuid) : integer;
+    public function index() : Collection;
+    public function show(integer $uuid) : Model;
 }
 
