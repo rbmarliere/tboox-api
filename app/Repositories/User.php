dbb7c46a3ff8e654767275193e07e3a2b0b89bd7
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-04 13:15:06 -0200

    basic repos

---
+++ app/Repositories/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+class User implements Repository
+{
+    public function createOrFail(array $fields)
+    {
+    }
+
+    public function destroyOrFail(integer $uuid)
+    {
+    }
+
+    public function index()
+    {
+    }
+
+    public function show(integer $uuid)
+    {
+    }
+}
+
