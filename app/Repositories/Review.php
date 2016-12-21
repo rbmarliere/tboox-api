a8be28484425b754b892b3a51191ec9336930c37
Ricardo Marliere <rbmarliere@gmail.com>
2016-12-21 16:55:13 -0200

    fixing review store

--- app/Repositories/Review.php
+++ app/Repositories/Review.php
@@ -3,12 +3,25 @@
 namespace App\Repositories;
 
 use App\Models\Review as M_Review;
+use App\Repositories\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Review extends BaseRepository
 {
-    public function __construct(M_Review $model)
+    public function __construct(M_Review $model, Collection $collection)
     {
         $this->model = $model;
+        $this->collection = $collection;
+    }
+
+    public function createOrFail(array $fields) : Model
+    {
+        $fields['uuid'] = Uuid::uuid4()->toString();
+        $fields['collection_id'] = $this->collection->show($fields['collection_id'])->id;
+
+        return $this->model
+            ->create($fields);
     }
 }
 
